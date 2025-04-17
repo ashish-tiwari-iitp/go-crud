a6fe3e54a9a5903f6a3b8ce0d5dc5ebb71b45886
ASHISH KUMAR TIWARI <143188246+ashish-tiwari-iitp@users.noreply.github.com>
2025-04-17 10:16:49 +0000

    added GMT time as well

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 )
 
 func main() {
-	// Load IST timezone
+	// Load IST and GMT timezones
 	istLocation, err := time.LoadLocation("Asia/Kolkata")
 	if err != nil {
 		fmt.Println("Error loading IST location:", err)
+		return
+	}
+
+	gmtLocation, err := time.LoadLocation("GMT")
+	if err != nil {
+		fmt.Println("Error loading GMT location:", err)
 		return
 	}
 
@@ -21,4 +27,7 @@ func main() {
 
 	// Format and print time in IST
 	fmt.Println("Current time in IST:", now.In(istLocation).Format("2006-01-02 15:04:05 MST"))
+
+	// Format and print time in GMT
+	fmt.Println("Current time in GMT:", now.In(gmtLocation).Format("2006-01-02 15:04:05 MST"))
 }
