64bf214dcdab1ec4d437246dcbd85726b314f673
ASHISH KUMAR TIWARI <143188246+ashish-tiwari-iitp@users.noreply.github.com>
2025-04-17 10:21:27 +0000

    added a game

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
-func main() {
+func printTimes() {
 	// Load IST and GMT timezones
 	istLocation, err := time.LoadLocation("Asia/Kolkata")
 	if err != nil {
@@ -22,12 +23,38 @@ func main() {
 	// Get current time
 	now := time.Now()
 
-	// Format and print time in UTC
+	// Print times
 	fmt.Println("Current time in UTC:", now.UTC().Format("2006-01-02 15:04:05 MST"))
-
-	// Format and print time in IST
 	fmt.Println("Current time in IST:", now.In(istLocation).Format("2006-01-02 15:04:05 MST"))
-
-	// Format and print time in GMT
 	fmt.Println("Current time in GMT:", now.In(gmtLocation).Format("2006-01-02 15:04:05 MST"))
+}
+
+func playGuessingGame() {
+	rand.Seed(time.Now().UnixNano())
+	secretNumber := rand.Intn(10) + 1 // number between 1 and 10
+	var guess int
+
+	fmt.Println("\n🎮 Let's play a guessing game!")
+	fmt.Println("I'm thinking of a number between 1 and 10. Can you guess it?")
+
+	for attempts := 0; attempts < 3; attempts++ {
+		fmt.Printf("Attempt %d: Enter your guess: ", attempts+1)
+		fmt.Scan(&guess)
+
+		if guess == secretNumber {
+			fmt.Println("🎉 Correct! You win!")
+			return
+		} else if guess < secretNumber {
+			fmt.Println("Too low!")
+		} else {
+			fmt.Println("Too high!")
+		}
+	}
+
+	fmt.Printf("😢 Sorry, you lost. The number was %d.\n", secretNumber)
+}
+
+func main() {
+	printTimes()
+	playGuessingGame()
 }
